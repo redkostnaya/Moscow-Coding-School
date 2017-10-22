3388ee05b5e808c1c12c616aa8d4457a2607f127
redkostnaya <nastasia.chernykh@gmail.com>
2017-10-22 12:34:21 +0300

    new changes

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// alert("Hello, world!");
+var myVariable;
+console.log(myVariable);
+
+myVariable = 10;
+console.log(myVariable);
+
+var myString = "Hello, world!";
+console.log( myString );
+
+var myBooLean = "true";
+console.log(myBooLean);
+
+var myNull = null;
+console.log( myNull );
+
+var myUndefined = undefined;
+console.log( myUndefined);
+
+
+
+var season = "spring";
+if (season === 'autumn') {
+  consol.log("Ура! Осень!");
+}
+else {
+  console.log('Не осень');
+}
+
+
+// продолжила на занятии тут//
